c5d68c9138ea1ac9015bfea17cad8aa856ec887c
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-10-29 14:34:37 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,30 @@
+'''
+DATE=29th oct 2022
+DAY= saturday
+TOPIC=new things 
+AUTHOR= kiranmai
+'''
+''' 
+Resume builder mod apk from google
+will get the templates 
+select anyone which u like 
+and fill the details  
+'''
+"""MAIN THINS MUST INCLUDE IN RESUME
+1.mail
+2.location
+3.github account
+4.linkedIn account
+5.phone number
+6.carrier objectives
+7.education
+8.skill(python,postgresql,msoffice(vlookup),communication skills etc) 
+9.language
+10.interests
+11.achievements"""
+
+"""TO KEEP CARRIER OBJECTIVES
+donot copy from others ,if u want the same thing in different formate
+QUILLBOT helps you"""
+
+
